80f27e103f0dbd49c19e3dafde2cbadc4e9321d3
sothiis <21424597@anhembimorumbi.edu.br>
2022-05-05 17:59:20 -0300

    testando

--- client/Cliente.java
+++ client/Cliente.java
@@ -23,16 +23,17 @@ public class Cliente {
     System.out.println("Opções:");
     System.out.println("1) Jogador Vs CPU:");
     System.out.println("2) Jogador Vs Jogador");
-    System.out.println("Digite 1 ou 2 para iniciar o jogo");
+    System.out.println("Digite CPU para jogar contra a CPU ou 02 para ");
     int escolha = Integer.parseInt(scanner.nextLine());
     /** AQUI PRECISA SER ESCOLHIDO O TIPO DE JOGO */
     System.out.println("O jogo irá começar, se prepare!");
     /* System.out.println("Agora você está jogando contra a CPU!"); */
     /* AQUI JOGO CONTRA CPU */
     /* System.out.println("Agora você está jogando contra outro jogador"); */
- 
+    System.out.println("(1) Pedra, (2) Papel, (3) Tesoura");
+
     /*PEDIDO CONEXÃO*/
-    if (tipoJogo == 1) { 
+    if (tipoJogo == 01) { 
             final int PORT = 1234;
             final String IP = "127.0.0.1";
             
@@ -44,7 +45,7 @@ public class Cliente {
             }
 
             
-        } else if (tipoJogo == 2) {
+        } else if (tipoJogo == 02) {
             final int PORT = 123456;
             final String IP = "127.0.0.1";
              
@@ -56,7 +57,7 @@ public class Cliente {
             }
         }
 
-/*COMUNICAÇÃO/REQUISIÇÃO*/
+/*COMUNICAÇÃO/REQUISIÇÃO/RESPOSTA*/
 
     Requisicao requisicao = new Requisicao();
 
